3eab7d5d3b65ab76c1a1fe61b30495327e4a962f
Elyse2500 <ielyse250@gmail.com>
2026-08-05 21:55:54 +0200

    Move hibernate config into HibernateUtil, remove hibernate.cfg.xml

--- src/main/java/com/auca/library/util/HibernateUtil.java
+++ src/main/java/com/auca/library/util/HibernateUtil.java
@@ -1,5 +1,6 @@
 package com.auca.library.util;
 
+import com.auca.library.domain.*;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -8,7 +9,31 @@ public class HibernateUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
-        return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        Configuration config = new Configuration();
+
+        // Database connection
+        config.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
+        config.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/auca_library_db");
+        config.setProperty("hibernate.connection.username", "postgres");
+        config.setProperty("hibernate.connection.password", "Elyse@1234");
+
+        // Hibernate settings
+        config.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        config.setProperty("hibernate.hbm2ddl.auto", "update");
+        config.setProperty("hibernate.show_sql", "true");
+        config.setProperty("hibernate.format_sql", "true");
+
+        // Entity mappings
+        config.addAnnotatedClass(Location.class);
+        config.addAnnotatedClass(User.class);
+        config.addAnnotatedClass(Book.class);
+        config.addAnnotatedClass(Shelf.class);
+        config.addAnnotatedClass(Room.class);
+        config.addAnnotatedClass(Borrower.class);
+        config.addAnnotatedClass(Membership.class);
+        config.addAnnotatedClass(MembershipType.class);
+
+        return config.buildSessionFactory();
     }
 
     public static SessionFactory getSessionFactory() {
